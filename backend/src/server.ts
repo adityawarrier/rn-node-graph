30a335ffb895904aaf8ec3772cb302ccf175b338
Aditya Warrier <aditya.warrier.unni2@gmail.com>
2021-12-18 18:07:20 +0530

    Added eslint, prettier for express

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,10 @@
+import cors from 'cors';
 import express from 'express';
 
 const app = express();
 
+app.use(cors());
+
 app.listen(1337, () => {
-  console.log('Up and Running!')
-})
+  console.log('Up and Running!');
+});
