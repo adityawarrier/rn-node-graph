b669d1ba71cab1d8720a97951d1e92a670de678e
Aditya Warrier <aditya.warrier.unni2@gmail.com>
2022-01-09 17:04:08 +0530

    Queries

--- backend/src/graphql/resolvers/index.ts
+++ backend/src/graphql/resolvers/index.ts
@@ -1,7 +1,24 @@
-import { UserList } from "../../data";
+import { MovieList, UserList } from "../../data";
 
 export const resolvers = {
   Query: {
     users: () => UserList,
+    user: (_, args) => {
+      const { id } = args;
+
+      const user = UserList.find((u) => u.id === parseInt(id, 10));
+      return user;
+    },
+
+    movies: () => MovieList,
+    movie: (_, args) => {
+      const { id } = args;
+
+      const movie = MovieList.find((m) => m.id === parseInt(id, 10));
+      return movie;
+    },
+  },
+  User: {
+    favouriteMovies: () => MovieList,
   },
 };
--- backend/src/graphql/typedefs/index.ts
+++ backend/src/graphql/typedefs/index.ts
@@ -1,16 +1,37 @@
 import { gql } from "apollo-server";
 
 const typeDefs = gql`
+  enum Nationality {
+    CANADA
+    BRAZIL
+    INDIA
+    GERMANY
+    CHILE
+    UKRAINE
+  }
+
   type User {
     id: ID!
     name: String!
     age: Int!
     userName: String!
-    nationality: String!
+    nationality: Nationality!
+    friends: [User!]
+    favouriteMovies: [Movie!]
+  }
+
+  type Movie {
+    id: ID!
+    name: String!
+    yearOfPublication: Int!
+    isInTheaters: Boolean!
   }
 
   type Query {
     users: [User!]!
+    user(id: ID!): User
+    movies: [Movie!]!
+    movie(id: ID!): Movie
   }
 `;
 
